3ebf7f508170d698cbe5805b2891560791a8711f
Petr Vacek <givelinx@centrum.cz>
2014-11-06 17:42:33 +0100

    Added primitive speeder implementation

---
+++ Assets/Scripts/Speeder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Speeder : MonoBehaviour {
+
+	public float force;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag.Equals("Speeder"))
+		{
+			var rb = this.GetComponentInParent<Rigidbody>();
+			rb.AddForce(this.transform.forward * force);
+		}
+	}
+}
