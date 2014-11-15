e7987d263629ae9f13d80818cc1566e3a1c3d2b7
Michal Kimle <kimle.michal@gmail.com>
2014-11-15 18:04:51 +0100

    changes to energy script and prefeb
    I had to move hovercraft from its original position for testing purposes because of the
    missing map. Feel free to move it back when the map is added.

--- Assets/Scripts/Energy.cs
+++ Assets/Scripts/Energy.cs
@@ -2,35 +2,37 @@
 using System.Collections;
 
 public class Energy : MonoBehaviour {
-	public int energy;
-	private int resetTimer;
-	public int resetTime;
-	private bool reset = false;
-	// Use this for initialization
-	void Start () {
-	
-	}
+  public int energy;
+  private int resetTimer;
+  public int resetTime;
+  private bool reset = false;
+  // Use this for initialization
+  void Start () {
 
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.gameObject.tag.Equals ("Hovercraft")) {
-			(other.transform.parent.gameObject.GetComponent("SimpleHovercraft") as SimpleHovercraft).AddEnergy(this.energy);
-            (other.transform.parent.gameObject.GetComponent("Player") as Player).energy += energy;
-		}
-		(this.transform.Find("Visual")).gameObject.SetActive(false);
-		(this.GetComponent("Collider") as Collider).enabled = true;
-		resetTimer = 0;
-		reset = true;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		if (reset) {
-			if (++resetTimer > resetTime) {
-				reset = false;
-				(this.transform.Find("Visual")).gameObject.SetActive(true);
-				(this.GetComponent("Collider") as Collider).enabled = true;
-			}
-		}
-	}
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.tag.Equals ("Hovercraft")) {
+      (other.transform.parent.gameObject.GetComponent("SimpleHovercraft") as SimpleHovercraft).AddEnergy(this.energy);
+      (other.transform.parent.gameObject.GetComponent("Player") as Player).energy += energy;
+    }
+    (this.GetComponent("Renderer") as ParticleRenderer).enabled = false;
+    (this.GetComponent("Light") as Light).enabled = false;
+    (this.GetComponent("Collider") as Collider).enabled = true;
+    resetTimer = 0;
+    reset = true;
+  }
+
+  // Update is called once per frame
+  void FixedUpdate () {
+    if (reset) {
+      if (++resetTimer > resetTime) {
+        reset = false;
+        (this.GetComponent("Renderer") as ParticleRenderer).enabled = true;
+        (this.GetComponent("Light") as Light).enabled = true;
+        (this.GetComponent("Collider") as Collider).enabled = true;
+      }
+    }
+  }
 }
