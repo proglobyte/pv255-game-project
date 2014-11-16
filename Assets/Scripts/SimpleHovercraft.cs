b210109aa7ce6445f3b6f9423e6a38157fdf4b7c
Michal Kimle <kimle.michal@gmail.com>
2014-11-16 12:18:22 +0100

    basic lighting settings and fixed hovercraft movement

--- Assets/Scripts/SimpleHovercraft.cs
+++ Assets/Scripts/SimpleHovercraft.cs
@@ -2,58 +2,44 @@
 using System.Collections;
 
 public class SimpleHovercraft : MonoBehaviour {
-	
-	Transform fan;
-	public float power;
-	public string player;
-	private float nextReset = 0.0f;
-	private float resetFrequency = 0.5f;
-	public int energy = 0;
-	public int maxEnergy = 100;
-	public float speed;
-	private const float speedConstant = 8;
 
-	private Vector3 lastPosition;
-	
-	void Start () {
-		fan = transform.Find("Fan");
-		lastPosition = this.transform.position;
-	}
+  Transform fan;
+  private Player player;
+  private float nextReset = 0.0f;
+  private float resetFrequency = 0.5f;
+  public float speed;
+  private const float speedConstant = 8;
 
-	public void AddEnergy(int energy) {
-		this.energy += energy;
-		if (this.energy > this.maxEnergy)
-			this.energy = this.maxEnergy;
-	}
+  private Vector3 lastPosition;
 
-	void FixedUpdate () {
-		
-		//float r = Input.GetAxis("Horizontal"+player) * -10;
-		
-		//fan.localRotation = Quaternion.Euler(0,r,0);
-		
-		float force = Input.GetAxis("Vertical"+player) * power;
-		
-		rigidbody.AddForceAtPosition( fan.forward * force, fan.position );
+  void Start () {
+      fan = transform.Find("Fan");
+      lastPosition = this.transform.position;
+      player = GetComponent<Player>();
+    }
 
-		float torqueForce = Input.GetAxis ("Horizontal" + player) * power * 4;
-		Debug.Log (torqueForce);
-		rigidbody.AddTorque (this.transform.up * torqueForce);
+  void FixedUpdate () {
+      //float r = Input.GetAxis("Horizontal"+player.id) * -10;
+      //fan.localRotation = Quaternion.Euler(0,r,0);
+      float force = Input.GetAxis("Vertical"+player.id) * player.power;
+      rigidbody.AddForceAtPosition( fan.forward * force, fan.position );
+      float torqueForce = Input.GetAxis ("Horizontal" + player.id) * player.power * 4;
+      Debug.Log (torqueForce);
+      rigidbody.AddTorque (this.transform.up * torqueForce);
+      this.speed = Vector3.Distance (lastPosition, this.transform.position) * speedConstant;
+      lastPosition = this.transform.position;
+    }
 
-		this.speed = Vector3.Distance (lastPosition, this.transform.position) * speedConstant;
-		lastPosition = this.transform.position;
-	}
-
-	void Update() {
-		if (Input.GetButton ("Reset"+player) && Time.time > nextReset ) {
-			nextReset = Time.time + resetFrequency;
-			var pos = this.transform.position;
-			pos.y += 50;
-			this.transform.position = pos;
-			var rot = this.transform.rotation;
-			rot.x = 0;
-			rot.z = 0;
-			this.transform.rotation = rot;
-		}
-	}
+  void Update() {
+      if (Input.GetButton ("Reset"+player.id) && Time.time > nextReset ) {
+            nextReset = Time.time + resetFrequency;
+            var pos = this.transform.position;
+            pos.y += 50;
+            this.transform.position = pos;
+            var rot = this.transform.rotation;
+            rot.x = 0;
+            rot.z = 0;
+            this.transform.rotation = rot;
+          }
+    }
 }
