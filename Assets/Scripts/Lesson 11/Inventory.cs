9f218046edf685c20a2a031780f0157dbb7ebb8c
Kiclus <kiclus6@gmail.com>
2015-01-17 19:44:48 +0100

    New texturing
    změna v game.scene přidání textur atd.

---
+++ Assets/Scripts/Lesson 11/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Inventory : MonoBehaviour {
+	public List<string> items;
+
+	public string GetItem(string comparedItem)
+	{
+		foreach(string item in items)
+			if(item == comparedItem)
+				return item;
+		return null;
+	}
+}
